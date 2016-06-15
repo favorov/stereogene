f82e124192d59975156066fcfb70c2933c8c341a
Alexander Favorov <favorov@sensi.org>
2016-06-15 08:05:51 +0300

    Integration works again

--- galaxy/stereogene-galaxy-simple.sh
+++ galaxy/stereogene-galaxy-simple.sh
@@ -1,21 +1,29 @@
 #!/bin/bash
 #$1 is chrom file
-#$2 is query
+#$2 is query file
 #$3 is query type
-#$4 is reference
+#$4 is reference file 
 #$5 is reference type 
-#$6 is window size 
-#$7 is pdf name as Galaxy asks for
+#$6 is window size
+#$7 is query long Galaxy name
+#$8 is reference long Galaxy name
+#$9 is pdf name as Galaxy asks for
+#$10 is NONE/BASE wigg?
+#$11 is wigg name for correlation track
 qname=${2}.${3}
 rname=${4}.${5}
 #stereogene looks at the extenson of the file
-ln $2 $qname
-ln $4 $rname
+ln -f $2 $qname
+ln -f $4 $rname
 #debug
 #echo StereoGene -s -chrom $1 $qname $rname >> ~/tmp/log-galaxy/simplecmd.log
-StereoGene -chrom $1 -wSize $6 -R $rname $qname > out.txt
+StereoGene -chrom $1 -wSize $6 -outWig ${10} -R $rname $qname > out.txt
 outname=`awk -e '/^out=\"(.+)\"/{match($_,"^out=\"(.+)\"",a);print a[1]}' out.txt`
-Rscript ${outname}.r
+Rscript ${outname}.r $7 $8 > /dev/null
 unlink $qname
 unlink $rname
-mv ${outname}.pdf $7
+mv ${outname}.pdf $9
+if [ ${10} != 'NONE' ] 
+then
+  mv ${outname}.wig ${11}
+fi
