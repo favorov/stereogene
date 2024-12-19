a9f6e4294b157b1829df814727f370183b68ba2f
Alexander Favorov <favorov@sensi.org>
2024-12-19 16:27:40 -0500

    Working config example for v2.50

--- example/example-cnf.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-./StereoGene cfg=conf.cfg H3K4me1.bed H3K4me3.bed 
-Rscript H3K4me1~H3K4me3_report.r
---
+++ example/example-with-config.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+../src/StereoGene cfg=conf.cfg H3K4me1.bed H3K4me3.bed
